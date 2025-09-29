fed36526b0c323cd4a19e227e629574ddd8ea54f
salma.ehab <salma.ehab@expertapps.com.sa>
2025-09-29 14:43:57 +0300

    finish Login ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,9 @@ android {
 }
 
 dependencies {
+    implementation("com.googlecode.libphonenumber:libphonenumber:8.13.20")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("com.hbb20:ccp:2.7.3")
     implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
---
+++ app/src/main/java/com/example/onboarding/presentaion/activity/LogInActivity.kt
@@ -0,0 +1,21 @@
+package com.example.onboarding.presentaion.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.onboarding.R
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_log_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
