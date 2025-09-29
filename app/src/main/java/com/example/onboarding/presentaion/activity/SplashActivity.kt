a101ef2a20dfa8be209fa61811308fa9070c511b
salma.ehab <salma.ehab@expertapps.com.sa>
2025-09-29 11:03:14 +0300

    Add splash activity+edit fragments ui

--- app/src/main/java/com/example/onboarding/presentaion/activity/SplashActivity.kt
+++ app/src/main/java/com/example/onboarding/presentaion/activity/SplashActivity.kt
@@ -1,21 +1,29 @@
 package com.example.onboarding.presentaion.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.onboarding.R
+import com.example.onboarding.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var  binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000
+        )
     }
 }
