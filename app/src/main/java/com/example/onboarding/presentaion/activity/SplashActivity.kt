051c81d4ea83a2941db5d0175786b5dff9807c8e
salma.ehab <salma.ehab@expertapps.com.sa>
2025-09-29 18:27:52 +0300

    splash ok

--- app/src/main/java/com/example/onboarding/presentaion/activity/SplashActivity.kt
+++ app/src/main/java/com/example/onboarding/presentaion/activity/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -31,5 +32,6 @@ class SplashActivity : AppCompatActivity() {
         finish()
             }, 3000
         )
+        Log.d("Splash","ok")
     }
 }
